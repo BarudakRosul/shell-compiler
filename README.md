aa49343077cb6514f172d27bbd625ab62481e9f7
Rafa Fazri <108041588+RFHackers@users.noreply.github.com>
2023-12-18 14:32:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Shell Compiler
+
+&emsp; <img src="https://raw.githubusercontent.com/BarudakRosul/shell-compiler/master/images/logo.svg" alt="Shell Compiler Logo" width="140"/>
+
+[![Release](https://img.shields.io/github/v/tag/BarudakRosul/shell-compiler?label=release)](https://github.com/BarudakRosul/shell-compiler/releases)
+[![License](https://img.shields.io/github/license/BarudakRosul/shell-compiler)](/LICENSE)
+[![Stars](https://img.shields.io/github/stars/BarudakRosul/shell-compiler)](https://github.com/BarudakRosul/shell-compiler/stargazers)
+[![Forks](https://img.shields.io/github/forks/BarudakRosul/shell-compiler)](https://github.com/BarudakRosul/shell-compiler/network/members)
+[![Issues](https://img.shields.io/github/issues/BarudakRosul/shell-compiler)](https://github.com/BarudakRosul/shell-compiler/issues)
+[![Pull Requests](https://img.shields.io/github/issues-pr/BarudakRosul/shell-compiler)](https://github.com/BarudakRosul/shell-compiler/pulls)
+
+## Daftar Isi
+
+1. [Pendahuluan](#pendahuluan)
+2. [Demo](#demo)
+3. [Fitur](#fitur)
+4. [Instalasi](#instalasi)
+5. [Berkontribusi](#berkontribusi)
+6. [Lisensi](#lisensi)
+7. [Penghargaan](#penghargaan)
+8. [Catatan Perubahan](#catatan-perubahan)
+
+## Pendahuluan
+
+Shell Compiler bertujuan menyediakan solusi yang aman dan serbaguna untuk mengenkripsi berbagai jenis skrip shell, termasuk Bourne Shell (`sh`), Bourne Again Shell (`bash`), Z Shell (`zsh`), Korn Shell (`ksh`), dan MirBSD Korn Shell (`mksh`), menggunakan perpustakaan OpenSSL. Alat ini memastikan kerahasiaan skrip shell, menambahkan lapisan perlindungan tambahan untuk kode yang sensitif.
+
+> [!WARNING]
+> Shell Compiler mungkin tidak kompatibel atau tidak didukung pada beberapa sistem, seperti pada Ultrix.
+
+## Demo
+
+![Screenshot 1](https://raw.githubusercontent.com/BarudakRosul/shell-compiler/master/images/screenshot_1.png)
+
+![Screenshot 2](https://raw.githubusercontent.com/BarudakRosul/shell-compiler/master/images/screenshot_2.png)
+
+![Screenshot 3](https://raw.githubusercontent.com/BarudakRosul/shell-compiler/master/images/screenshot_3.png)
+
+## Fitur
+
+Shell Compiler menawarkan fitur-fitur berikut:
+
+- **Enkripsi Aman**: Memanfaatkan OpenSSL untuk enkripsi skrip shell yang kuat dan aman.
+- **Dukungan untuk Berbagai Shell**: Mengenkripsi berbagai jenis skrip shell, termasuk sh, bash, zsh, ksh, dan mksh.
+- **Antarmuka Ramah Pengguna**: Antarmuka sederhana dan intuitif untuk proses enkripsi yang mudah.
+- **Kustomisasi**: Memungkinkan pengguna menyesuaikan pengaturan enkripsi sesuai dengan kebutuhan keamanan mereka.
+- **Kompatibilitas Cross-Platform**: Bekerja dengan lancar di berbagai sistem operasi dan lingkungan.
+
+## Instalasi
+
+Untuk menyiapkan Shell Compiler secara lokal, ikuti langkah-langkah instalasi ini:
+
+1. Kloning repositori:
+
+   ```shell
+   git clone https://github.com/BarudakRosul/shell-compiler
+   ```
+
+2. Buka direktori repo:
+
+   ```shell
+   cd shell-compiler
+   ```
+
+3. Instal paket:
+   - Untuk Termux:
+     ```shell
+     pkg update -y && pkg upgrade -y
+     ```
+     ```shell
+     pkg install $(cat package.txt) -y
+     ```
+   - Untuk Linux:
+     ```shell
+     apt update -y && apt upgrade -y
+     ```
+     ```shell
+     apt-get install $(cat package.txt) -y
+     ```
+
+4. Jalankan aplikasi:
+
+   ```shell
+   bash shell-compiler.sh
+   ```
+
+## Berkontribusi
+
+Kontribusi pada Proyek Kompilator Shell sangat dihargai! Baik melaporkan bug, menyarankan fitur baru, atau berkontribusi pada perbaikan kode.
+
+## Lisensi
+
+Shell Compiler dilisensikan di bawah Lisensi Apache-2.0 - lihat berkas [LICENSE](/LICENSE) untuk detailnya.
+
+## Penghargaan
+
+Shell Compiler menghargai dukungan dan kontribusi dari individu dan proyek sumber terbuka berikut:
+
+- [Librari OpenSSL](https://www.openssl.org) - Untuk menyediakan landasan yang aman untuk enkripsi.
+- @RFHackers - Pengembang utama dan pencipta aplikasi.
+- Komunitas sumber terbuka - Untuk kontribusi berharga pada alat dan perpustakaan yang digunakan dalam proyek ini.
+
+## Catatan Perubahan
+
+Terus ikuti perubahan dan pembaruan terbaru Shell Compiler dengan mengacu ke [Catatan Perubahan](https://github.com/BarudakRosul/shell-compiler/releases).
+
+Terima kasih telah memilih Shell Compiler! Kami bertujuan untuk memberikan solusi yang aman dan andal untuk mengenkripsi skrip shell di berbagai lingkungan.
---
+++ shell-compiler.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+
+MODE_ENC=("aes-128-cbc" "aes-192-cbc" "aes-256-cbc" "aria-128-cbc" "aria-192-cbc" "aria-256-cbc" \
+"base64" "camellia-128-cbc" "camellia-192-cbc" "camellia-256-cbc" "zlib")
+VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo null)
+
+# The [[ -t 1 ]] check only works when the function is not called from
+# a subshell (like in `$(...)` or `(...)`, so this hack redefines the
+# function at the top level to always return false when stdout is not
+# a tty.
+if [[ -t 1 ]]; then
+  is_tty() {
+    true
+  }
+else
+  is_tty() {
+    false
+  }
+fi
+
+setup_color() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    BLACK=""
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    MAGENTA=""
+    CYAN=""
+    WHITE=""
+    BOLD=""
+    ITALIC=""
+    UNDERLINE=""
+    RESET=""
+    return
+  fi
+
+  BLACK=$(printf '\033[90m')
+  RED=$(printf '\033[91m')
+  GREEN=$(printf '\033[92m')
+  YELLOW=$(printf '\033[93m')
+  BLUE=$(printf '\033[94m')
+  MAGENTA=$(printf '\033[95m')
+  CYAN=$(printf '\033[96m')
+  WHITE=$(printf '\033[97m')
+  BOLD=$(printf '\033[1m')
+  ITALIC=$(printf '\033[3m')
+  UNDERLINE=$(printf '\033[4m')
+  RESET=$(printf '\033[0m')
+}
+
+info() {
+  echo "${RESET}${BOLD}${GREEN}Info:${WHITE} $@${RESET}"
+}
+
+error() {
+  echo "${RESET}${BOLD}${RED}Error: $@${RESET}"
+}
+
+banner() {
+  echo "${RESET}${BOLD}${RED}╔═════════════════════════════════════════════════════════╗
+║${YELLOW}        ┏━┓╻ ╻┏━╸╻  ╻     ┏━╸┏━┓┏┳┓┏━┓╻╻  ┏━╸┏━┓         ${RED}║
+║${YELLOW}        ┗━┓┣━┫┣╸ ┃  ┃  ╺━╸┃  ┃ ┃┃┃┃┣━┛┃┃  ┣╸ ┣┳┛         ${RED}║
+║${YELLOW}        ┗━┛╹ ╹┗━╸┗━╸┗━╸   ┗━╸┗━┛╹ ╹╹  ╹┗━╸┗━╸╹┗╸         ${RED}║
+╚═════════════════════════════════════════════════════════╝
+${CYAN}  Author : ${WHITE}Barudak Rosul
+${CYAN}  Version: ${WHITE}$VERSION
+${CYAN}  GitHub : ${WHITE}${UNDERLINE}https://github.com/BarudakRosul${RESET}"
+}
+
+main_menu_type_shell() {
+  trap "echo; error 'Program cancelled!'; exit 127;" INT TERM ERR
+
+  clear
+  banner
+  echo "${RESET}${BOLD}${RED}╔═════════════════════════════════════════════════════════╗
+║${YELLOW}          Choose your type shell for encrypted           ${RED}║
+╚═════════════════════════════════════════════════════════╝
+${GREEN}  1. ${WHITE}bash (Bourne Again Shell)
+${GREEN}  2. ${WHITE}sh (Bourne Shell)
+${GREEN}  3. ${WHITE}zsh (Z Shell)
+${GREEN}  4. ${WHITE}ksh (Korn Shell)
+${GREEN}  5. ${WHITE}mksh (MirBSD Korn Shell)
+${RED}  0. Exit
+╔═════════════════════════════════════════════════════════╗
+╚═════════════════════════════════════════════════════════╝"
+  read -p "${RESET}${BOLD}${WHITE}Choose ${GREEN}>>${RESET} " response
+  case $response in
+    0 | 00) info "The program exited by user!"; exit 0;;
+    1 | 01) TYPE_SHELL="bash";;
+    2 | 02) TYPE_SHELL="sh";;
+    3 | 03) TYPE_SHELL="zsh";;
+    4 | 04) TYPE_SHELL="ksh";;
+    5 | 05) TYPE_SHELL="mksh";;
+    "" | " ") error "Please select a menus!"; exit 1;;
+    *) error "Wrong input! Please try again!"; exit 1;;
+  esac
+
+  trap - INT TERM ERR
+}
+
+main_menu_mode() {
+  trap "echo; error 'Program cancelled!'; exit 127;" INT TERM ERR
+
+  local response
+  local number
+  local space
+  clear
+  banner
+  echo "${RESET}${BOLD}${RED}╔═════════════════════════════════════════════════════════╗
+║${YELLOW}            Choose your mode for encrypted               ${RED}║
+╚═════════════════════════════════════════════════════════╝${RESET}"
+  for ((i=0;i<${#MODE_ENC[@]};i++)); do
+    number=$((i + 1))
+    space=""
+    if (( 1 == ${#number} )); then
+      space=" "
+    fi
+    echo "${RESET}${BOLD}${GREEN} ${space}$((number)). ${WHITE}${MODE_ENC[i]}${RESET}"
+  done
+  echo "${RESET}${BOLD}${RED}  0. Exit
+╔═════════════════════════════════════════════════════════╗
+╚═════════════════════════════════════════════════════════╝${RESET}"
+  read -p "${RESET}${BOLD}${WHITE}Choose ${GREEN}>>${RESET} " response
+  if (( ${#response} == 2 )); then
+    response=${response##0}
+  fi
+  if (( $response > ${#MODE_ENC[@]} )); then
+    error "Wrong input! Please try again!"
+    exit 1
+  fi
+  case $response in
+    0 | 00) info "The program exited by user!"; exit 0;;
+    [0-9] | [0-9][0-9]) MODE=${MODE_ENC[response - 1]};;
+    "" | " ") error "Please select a menus!"; exit 1;;
+    *) error "Wrong input! Please try again!"; exit 1;;
+  esac
+
+  trap - INT TERM ERR
+}
+
+main_menu_input_file() {
+  trap "echo; error 'Program cancelled!'; exit 127;" INT TERM ERR
+
+  clear
+  banner
+  echo "${RESET}${BOLD}${RED}╔═════════════════════════════════════════════════════════╗
+║${YELLOW}      Input your file in \"file\" folder for encrypt       ${RED}║
+╚═════════════════════════════════════════════════════════╝
+${WHITE}  File type   : ${GREEN}${UNDERLINE}$TYPE_SHELL${RESET}${BOLD}
+${WHITE}  Encrypt mode: ${GREEN}${UNDERLINE}$MODE${RESET}${BOLD}
+${RED}╔═════════════════════════════════════════════════════════╗
+╚═════════════════════════════════════════════════════════╝${RESET}"
+  read -p "${RESET}${BOLD}${WHITE}Script ${GREEN}>>${RESET} " input
+
+  if [[ $input == "" || $input == " " ]]; then
+    error "Please input your file name!"
+    exit 1
+  elif ! [[ -f file/$input || -r file/$input ]]; then
+    error "No such file \"$input\" in directory \"file\""
+    exit 1
+  elif [[ -f out/$input || -r out/$input ]]; then
+    error "The file \"$input\" is already compressed!"
+    exit 1
+  fi
+
+  INPUT=$input
+
+  trap - INT TERM ERR
+}
+
+main() {
+  setup_color
+  main_menu_type_shell
+  main_menu_mode
+  main_menu_input_file
+  bash lib/${MODE}.sh -t ${TYPE_SHELL} -i file/${INPUT} -o out/${INPUT}
+}
+
+for ((i=0;i<${#MODE_ENC[@]};i++)); do
+  test -f lib/${MODE_ENC[i]}.sh && test -r lib/${MODE_ENC[i]}.sh || {
+    setup_color
+    error "Some files are missing!"
+    info "Please install the script from github <https://github.com/BarudakRosul/shell-compiler>"
+    exit 1
+  }
+done
+
+main
