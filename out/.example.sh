44692f4b8fe534c841a6ed6a317f4a80ef4b9dd4
Citra Bella Rahayu <146302496+Achixz@users.noreply.github.com>
2023-12-20 05:01:32 +0700

    chore: Update the encrypted code example

---
+++ out/.example.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+# This file encrypted at Tue Jan  1 00:00:04 WIB 1980 by
+# +------------------------------------------------------+
+# | Author: Barudak Rosul                                |
+# | Code: https://github.com/BarudakRosul/shell-compiler |
+# +------------------------------------------------------+
+# Don't forget follow me on:
+# -  https://github.com/BarudakRosul
+# Sponsor this project:
+# -  https://buymeacoffee.com/fajarkim
+# -  https://trakteer.id/fajarkim
+#
+skip=70
+tab='	'
+nl='
+'
+IFS=" $tab$nl"
+
+# Make sure important variables exist if not already defined
+# $USER is defined by login(1) which is not always executed (e.g. containers)
+# POSIX: https://pubs.opengroup.org/onlinepubs/009695299/utilities/id.html
+USER=${USER:-$(id -u -n)}
+# $HOME is defined at the time of login, but it could be unset. If it is unset,
+# a tilde by itself (~) will not be expanded to the current user's home directory.
+# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# macOS does not have getent, but this works even if $HOME is unset
+HOME="${HOME:-$(eval echo ~$USER)}"
+umask=`umask`
+umask 77
+
+shctmpdir=
+trap 'res=$?
+  test -n "$shctmpdir" && rm -fr "$shctmpdir"
+  (exit $res); exit $res
+' 0 1 2 3 5 10 13 15
+
+case $TMPDIR in
+  / | */tmp/) test -d "$TMPDIR" && test -w "$TMPDIR" && test -x "$TMPDIR" || TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
+  */tmp) TMPDIR=$TMPDIR/; test -d "$TMPDIR" && test -w "$TMPDIR" && test -x "$TMPDIR" || TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
+  *:* | *) TMPDIR=$HOME/.cache/; test -d "$HOME/.cache" && test -w "$HOME/.cache" && test -x "$HOME/.cache" || mkdir "$HOME/.cache";;
+esac
+if type mktemp >/dev/null 2>&1; then
+  shctmpdir=`mktemp -d "${TMPDIR}shctmpXXXXXXXXX"`
+else
+  shctmpdir=${TMPDIR}shctmp$$; mkdir $shctmpdir
+fi || { (exit 127); exit 127; }
+
+shctmp=$shctmpdir/$0
+case $0 in
+-* | */*'
+') mkdir -p "$shctmp" && rm -r "$shctmp";;
+*/*) shctmp=$shctmpdir/`basename "$0"`;;
+esac || { (exit 127); exit 127; }
+
+case `printf 'X\n' | tail -n +1 2>/dev/null` in
+X) tail_n=-n;;
+*) tail_n=;;
+esac
+if tail $tail_n +$skip <"$0" | openssl enc -aes-128-cbc -d -pbkdf2 -iter 10000 -k "ossl_kqvkxaXKImyAaxjcrW26r43MG5" > "$shctmp"; then
+  umask $umask
+  chmod 700 "$shctmp"
+  (sleep 5; rm -fr "$shctmpdir") 2>/dev/null &
+  "$shctmp" ${1+"$@"}; res=$?
+else
+  printf >&2 '%s\n%s\n' "Cannot decompress ${0##*/}" "Report bugs to <barudakrosul22garut@gmail.com>."
+  (exit 127); res=127
+fi; exit $res
+Salted__Ö!eør
+C!ö‡Ð‚‹°°Æ…ç<Ó=þöž()šØ™“šf\¾“‡
