cbd08853cd3c6db43ddd3d52a81c24fdd9ab37b0
Fatimah Azzahra <146313531+fatimazq@users.noreply.github.com>
2023-12-25 11:23:14 +0700

    chore: update installing packages

--- install.sh
+++ install.sh
@@ -2,13 +2,20 @@
 
 install_pkg_on_termux() {
   pkg update -y && pkg upgrade -y
-  pkg install git openssl-tool xz-utils jq ccrypt nodejs -y
+  pkg install build-essential openssl openssl-tool zlib zlib-static git xz-utils jq ccrypt nodejs make clang perl -y
+  cd
+  wget https://www.openssl.org/source/openssl-3.2.0.tar.gz
+  tar -xf openssl-3.2.0.tar.gz
+  cd openssl-3.2.0
+  ./config --prefix=$PREFIX --openssldir=$PREFIX zlib enable-zstd zlib-dynamic enable-zstd-dynamic shared
+  make && make install
+  cd
   npm -g install @barudakrosul/gcrypt
 }
 
 install_pkg_on_linux() {
   sudo apt update -y && sudo apt upgrade -y
-  sudo apt install build-essential checkinstall zlib1g-dev tar wget git xz-utils jq ccrypt nodejs make -y
+  sudo apt install build-essential checkinstall zlib1g-dev tar wget openssl git xz-utils jq ccrypt nodejs make clang perl -y
   cd /usr/local/src
   sudo wget https://www.openssl.org/source/openssl-3.2.0.tar.gz
   sudo tar -xf openssl-3.2.0.tar.gz
@@ -17,8 +24,6 @@ install_pkg_on_linux() {
   sudo make && sudo make install
   cd
   npm -g install @barudakrosul/gcrypt
-  openssl version
-  openssl
 }
 
 case $HOME in
