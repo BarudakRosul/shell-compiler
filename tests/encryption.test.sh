a69eb7eb932bd6ccedb879711a4a637d8c38a596
Andi Rahek <146312574+Andi-Rahek@users.noreply.github.com>
2023-12-30 14:16:28 +0700

    feat: Added des, des-cbc, desx, and desx-cbc mode (#17)

--- tests/encryption.test.sh
+++ tests/encryption.test.sh
@@ -6,7 +6,8 @@ export NODE_OPTIONS=--openssl-legacy-provider
 MODE_ENC=("aes-128-cbc" "aes-192-cbc" "aes-256-cbc" "aes-128-cbc_base64" "aes-192-cbc_base64" \
 "aes-256-cbc_base64" "aes-128-cbc_zlib" "aes-192-cbc_zlib" "aes-256-cbc_zlib" "aria-128-cbc" \
 "aria-192-cbc" "aria-256-cbc" "base64" "camellia-128-cbc" "camellia-192-cbc" "camellia-256-cbc" \
-"ccrypt" "des3" "des-ede" "des-ede3" "des-ede-cbc" "des-ede3-cbc" "gcrypt" "sm4" "sm4-cbc" "zlib")
+"ccrypt" "des" "des-cbc" "des-ede" "des-ede-cbc" "des-ede3" "des-ede3-cbc" "des3" "desx" "desx-cbc" \
+"gcrypt" "sm4" "sm4-cbc" "zlib")
 
 rm -rf file/* out/*
 echo "[!] Testing code for bash script file"
